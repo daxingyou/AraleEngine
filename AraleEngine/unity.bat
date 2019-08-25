204f7496607c896c59759aa5a044f5c5f827c3ba
unknown <411635116@qq.com>
2019-08-25 21:57:47 +0800

    优化.bat脚本

--- AraleEngine/unity.bat
+++ AraleEngine/unity.bat
@@ -1,31 +1,36 @@
-::jenkins cmd
-::set PUBLISH=E:/FTP/%BUILD_NUMBER%
-::set LOGPATH="%WORKSPACE%/unitylog.txt"
-::set ARGS="{'publish':'%PUBLISH%','configFile':'%WORKSPACE%/configFile','appVer':'%appVer%','resVer':'%resVer%','obb':'%obb%'}"
-::set PUBLISH="%PUBLISH%"
-::call "E:\AraleEngine\AraleEngine\AraleEngine\unity.bat" %LOGPATH% %ARGS% %PUBLISH%
+::=====================jenkins cmd=======================
+::set OUTPATH=E:/FTP/%BUILD_NUMBER%
+::call "E:\AraleEngine\AraleEngine\AraleEngine\unity.bat"
+::=======================================================
 echo off
 tasklist|find /i "unity.exe" && echo build failed, unity is running && pause && exit 1
 ::set UNITY depend on your env
 set UNITY="C:\Program Files\Unity\Editor\Unity.exe"
 set BATPATH=%~dp0
 set UNITYPROC="%BATPATH%"
-set ANDROIDPROC=%BATPATH%/../Android
-set LOGPATH=%1
-set ARGS=%2
-set OUTPATH=%3
-::run unity.bat directly
-if not defined LOGPATH set LOGPATH="unitylog.txt"
-if not defined ARGS set ARGS="{}"
-if not defined OUTPATH set OUTPATH=%BATPATH%\..\Publish
+set ANDROIDPROC=%BATPATH%\..\Android
+set JENKINS=%WORKSPACE%
+if not defined OUTPATH (
+	::run unity.bat directly
+	set OUTPATH=%BATPATH%\..\Publish
+	set JENKINS=%BATPATH%
+	set appVer=1.0.0
+	set resVer=1
+	set obb=false
+)
+
+set CONFIGFILE=%JENKINS%/configFile
+set LOGPATH="%JENKINS%/unitylog.txt"
+set ARGS=ARGS="{'publish':'%OUTPATH%','configFile':'%CONFIGFILE%','appVer':'%appVer%','resVer':'%resVer%','obb':'%obb%'}"
 ::export unity proc
 echo on
 %UNITY% -batchmode -quit -nographics -executeMethod BatchBuild.buildAndroid -projectPath %UNITYPROC% -logFile %LOGPATH% %ARGS%
 ::build apk
 cd %ANDROIDPROC%
 call gradlew assembleRelease
-::copy result to output
-if not exist %OUTPATH% md %OUTPATH%
+::copy apk to output
+if not exist "%OUTPATH%" md "%OUTPATH%"
 set DATETIME=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-move %ANDROIDPROC%\app\build\outputs\apk\release\app-release.apk %OUTPATH%/release_%DATETIME%.apk
+set APKPATH=%OUTPATH%/release_%DATETIME%.apk
+move %ANDROIDPROC%\app\build\outputs\apk\release\app-release.apk %APKPATH%
 pause
