3ca4c46726bbdcf4b02ca3fd67f220640436720c
unknown <411635116@qq.com>
2019-10-20 21:13:30 +0800

    开发工具更新

--- AraleEngine/unity.bat
+++ AraleEngine/unity.bat
@@ -5,7 +5,7 @@
 echo off
 tasklist|find /i "unity.exe" && echo build failed, unity is running && pause && exit 1
 ::set UNITY depend on your env
-set UNITY="C:\Program Files\Unity\Editor\Unity.exe"
+set UNITY="D:\YiYouClient\soft\Unity\Editor\Unity.exe"
 set BATPATH=%~dp0
 set UNITYPROC="%BATPATH%"
 set ANDROIDPROC=%BATPATH%\..\Android
@@ -14,23 +14,25 @@ if not defined OUTPATH (
 	::run unity.bat directly
 	set OUTPATH=%BATPATH%\..\Publish
 	set JENKINS=%BATPATH%
-	set appVer=1.0.0
-	set resVer=1
+	set version=1.0.1
 	set obb=false
+	set debug=false
 )
 
-set CONFIGFILE=%JENKINS%/configFile
+set CONFIGFILE=%JENKINS%/ConfigFile
 set LOGPATH="%JENKINS%/unitylog.txt"
-set ARGS=ARGS="{'publish':'%OUTPATH%','configFile':'%CONFIGFILE%','appVer':'%appVer%','resVer':'%resVer%','obb':'%obb%'}"
+set ARGS="{'publish':'%OUTPATH%','configFile':'%CONFIGFILE%','version':'%version%','obb':'%obb%','debug':'%debug%'}"
 ::export unity proc
 echo on
 %UNITY% -batchmode -quit -nographics -executeMethod BatchBuild.buildAndroid -projectPath %UNITYPROC% -logFile %LOGPATH% %ARGS%
 ::build apk
-cd %ANDROIDPROC%
+cd /d %ANDROIDPROC%
 call gradlew assembleRelease
 ::copy apk to output
 if not exist "%OUTPATH%" md "%OUTPATH%"
 set DATETIME=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 set APKPATH=%OUTPATH%/release_%DATETIME%.apk
 move %ANDROIDPROC%\app\build\outputs\apk\release\app-release.apk %APKPATH%
+::show download url
+echo FTP=http://192.168.31.20:280/FTP/%BUILD_NUMBER%/
 pause
